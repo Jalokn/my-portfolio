c75e588ba96bdc3d66e27d20f1a3575e54bf9c26
Jacob Knudson <jacoblknudson@gmail.com>
2022-07-13 09:30:18 -0600

    Failed to update navbar :(

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,39 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome, AiOutlineUser, AiOutlineMessage } from "react-icons/ai";
 import { RiServiceLine } from "react-icons/ri";
 import { BiBook } from "react-icons/bi";
 
 const Nav = () => {
-  useEffect(() => {
-    document.addEventListener("scroll", () => {
-      let scrollTopValue = Math.round(window.scrollY);
-      switch (scrollTopValue) {
-        case scrollTopValue < 1089 && scrollTopValue > 0:
-          setActiveNav("#home");
-          break;
-        case scrollTopValue < 1756 && scrollTopValue > 1089:
-          setActiveNav("#about");
-          break;
-        case scrollTopValue < 3232 && scrollTopValue > 1756:
-          setActiveNav("#experience");
-          break;
-        case scrollTopValue < 4919 && scrollTopValue > 3232:
-          setActiveNav("#portfolio");
-          break;
-        case scrollTopValue >= 4919:
-          setActiveNav("#contact");
-          break;
-        default:
-          break;
-      }
-      console.log(scrollTopValue);
-      return () => {
-        document.removeEventListener("scroll", () => {});
-      };
-    });
-  });
-
   const [activeNav, setActiveNav] = useState("#home");
   const handleNav = (navItem) => {
     setActiveNav(navItem);
